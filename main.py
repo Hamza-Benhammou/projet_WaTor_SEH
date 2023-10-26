402cfa53efe0c0e72b66a4ccd35232c2f641c845
Elie LECAS <elielecas00@gmail.com>
2023-10-26 16:28:12 +0200

    jeudi soir Elie

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ class Planet:
     def __init__(self,largeur_de_la_grille, hauteur_de_la_grille):
         self.largeur_de_la_grille = largeur_de_la_grille
         self.hauteur_de_la_grille = hauteur_de_la_grille
+        self.display_planet = [[ 0 for hauteur in range(self.largeur_de_la_grille)] for largeur in range(self.hauteur_de_la_grille)] 
 
-    def display_planet(self): 
-      self.display_planet = [[ 0 for hauteur in range(self.largeur_de_la_grille)] for largeur in range(self.hauteur_de_la_grille)]  
-      for ligne in self.display_planet:
-        print(*ligne)
+    def display(self): 
+        for ligne in self.display_planet:
+            print(*ligne)
+        print("\n-------------------\n")
 
 
 class Poisson:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, Planet ):
+        self.x = random.choice(range(Planet.largeur_de_la_grille))
+        self.y = random.choice(range(Planet.hauteur_de_la_grille))
+        self.Planet = Planet
 
 
     def deplacement(self):
-        self.x = random.choice(range(Planet.largeur_de_la_grille))
-        self.y = random.choice(range(Planet.hauteur_de_la_grille))
+        # self.x = random.choice(range(Planet.largeur_de_la_grille))
+        # self.y = random.choice(range(Planet.hauteur_de_la_grille))
         valeur_poisson =  "🐟"
         Planet.display_planet[self.y % Planet.hauteur_de_la_grille][self.x % Planet.largeur_de_la_grille] = valeur_poisson
         
@@ -45,28 +47,28 @@ class Poisson:
             Planet.display_planet[self.y % Planet.hauteur_de_la_grille][self.x % Planet.largeur_de_la_grille] = valeur_poisson
             Planet.display_planet[(self.y + 1) % Planet.hauteur_de_la_grille][self.x % Planet.largeur_de_la_grille] = 0
 
-
-# charge de reproduction
-        charge +=1
-
-        for ligne in grille:
+        for ligne in Planet.display_planet:
             print(*ligne)
         print("-------------------------------")
+
+
+# charge de reproduction
+        # charge +=1
+
+        # for ligne in grille:
+        #     print(*ligne)
+        # print("-------------------------------")
             
 
 
 
 planete_1 = Planet(10, 10)
 # planete_1.display_planet()
-poisson_1 = Poisson(5,5)
+poissons = [Poisson(planete_1) for _ in range(5)]
 
 chronon = 0
 
 while chronon < 50:
     poisson_1.deplacement()
-
-
-    
-    
-
-chronon += 1
+    planete_1.display()
+    chronon += 1
