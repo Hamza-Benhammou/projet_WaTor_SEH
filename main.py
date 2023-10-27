bbd5374e25487cc40fd2d1b76d4b0af9a1381319
Elie LECAS <elielecas00@gmail.com>
2023-10-27 16:10:13 +0200

    Elie vendredi 27 octobre

--- main.py
+++ main.py
@@ -2,35 +2,55 @@ import random
 import os
 import time
 
+# import copy
+# copy.deepcopy()
+
 class Planet:
     def __init__(self, largeur_de_la_grille, hauteur_de_la_grille):
         self.largeur_de_la_grille = largeur_de_la_grille
         self.hauteur_de_la_grille = hauteur_de_la_grille
-        self.display_planet = [[0 for hauteur in range(self.largeur_de_la_grille)] for largeur in range(self.hauteur_de_la_grille)]
+        self.grille = [[0 for hauteur in range(self.largeur_de_la_grille)] for largeur in range(self.hauteur_de_la_grille)]
+        
+
+    def peupler_le_monde(self, nombre_poissons):
+        self.poissons = []
+        self.poissons.extend(Poisson() for _ in range(nombre_poissons))
+        # Poisson() for _ in range(nombre_poissons)
+
+        # random.choice(range(planet.largeur_de_la_grille))
+        # random.choice(range(planet.hauteur_de_la_grille))
 
     def display(self):
-        for ligne in self.display_planet:
+        for ligne in self.grille:
             print(*ligne)
         
 
     def est_case_vide(self, x, y):
-        return self.display_planet[y % self.hauteur_de_la_grille][x % self.largeur_de_la_grille] == 0
+        return self.grille[y % self.hauteur_de_la_grille][x % self.largeur_de_la_grille] == 0
 
-    def deplacer_poisson(self, x_initial, y_initial, x_nouveau, y_nouveau, valeur_poisson):
-        self.display_planet[y_initial % self.hauteur_de_la_grille][x_initial % self.largeur_de_la_grille] = 0
-        self.display_planet[y_nouveau % self.hauteur_de_la_grille][x_nouveau % self.largeur_de_la_grille] = valeur_poisson
+    def mettre_a_jour_case(self, x_initial, y_initial, x_nouveau, y_nouveau, valeur_poisson):
+        self.grille[y_initial % self.hauteur_de_la_grille][x_initial % self.largeur_de_la_grille] = 0
+        self.grille[y_nouveau % self.hauteur_de_la_grille][x_nouveau % self.largeur_de_la_grille] = valeur_poisson
+
+    def simuler(self, duree):
+        for chronon in range(duree):
+            os.system('clear')
+            self.display()
+            for poisson in self.poissons:
+                poisson.deplacement()
+            time.sleep(.5)
 
 class Poisson:
-    def __init__(self, planet):
-        self.x = random.choice(range(planet.largeur_de_la_grille))
-        self.y = random.choice(range(planet.hauteur_de_la_grille))
+    def __init__(self, x,y):
+        self.x = x 
+        self.y = y 
         self.planet = planet
         self.age = 0
 
     def deplacement(self):
         deplacement_possible = [[self.x + 1, self.y], [self.x - 1, self.y], [self.x, self.y + 1], [self.x, self.y - 1]]
         nouveau_x, nouveau_y = self.choisir_deplacement(deplacement_possible)
-        if self.deplacer_poisson(nouveau_x, nouveau_y):
+        if self.deplacer_sur_planete(nouveau_x, nouveau_y):
             self.age += 1
         if self.age == 8:
             self.reproduction()
@@ -42,10 +62,10 @@ class Poisson:
         nouveau_y = deplacement_choisi[1]
         return nouveau_x, nouveau_y
 
-    def deplacer_poisson(self, nouveau_x, nouveau_y):
-        valeur_poisson = "🐟"
+    def deplacer_sur_planete(self, nouveau_x, nouveau_y):
+        valeur_poisson = '\U0001f41f'
         if self.planet.est_case_vide(nouveau_x, nouveau_y):
-            self.planet.deplacer_poisson(self.x, self.y, nouveau_x, nouveau_y, valeur_poisson)
+            self.planet.mettre_a_jour_case(self.x, self.y, nouveau_x, nouveau_y, valeur_poisson)
             self.x = nouveau_x
             self.y = nouveau_y
             return True
@@ -59,43 +79,13 @@ class Poisson:
                 new_poisson = Poisson(self.planet)
                 new_poisson.x = nouveau_x
                 new_poisson.y = nouveau_y
-                new_poisson.deplacer_poisson(nouveau_x, nouveau_y)
+
+                new_poisson.deplacer_sur_planete(nouveau_x, nouveau_y)
 
 
 
 
-def monde_1():
-    planete_1 = Planet(10, 10)
-    poissons = [Poisson(planete_1) for _ in range(1)]
+planete_1 = Planet(30, 30)
+planete_1.peupler_le_monde(5)
 
-    chronon = 0
-
-    while chronon < 50:
-        os.system('clear')
-        planete_1.display()
-        for poisson in poissons:
-            poisson.deplacement()
-
-        chronon += 1
-        time.sleep(.5)
-
-# Appel du monde 1
-monde_1()
-
-def monde_2():
-    planete_1 = Planet(30, 30)
-    poissons = [Poisson(planete_1) for _ in range(10)]
-
-    chronon = 0
-
-    while chronon < 100:
-        os.system('clear')
-        planete_1.display()
-        for poisson in poissons:
-            poisson.deplacement()
-
-        chronon += 1
-        time.sleep(.5)
-
-# Appel du monde 1
-# monde_2()
+planete_1.simuler(50)
